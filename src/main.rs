638ae1872dc01127d40818a007322879faea612c
EvanLuo42 <ziyun.luo@icloud.com>
2022-05-26 07:54:51 +0800

    feat: Improved the error handle.

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,51 @@ use clap::{Arg, Command};
 use data_encoding::{BASE64, Encoding, HEXLOWER};
 
 fn encode_or_decode(action: &str, string: &str, coder: Encoding) {
+    let action_undefined_error = command().error(
+        clap::ErrorKind::InvalidValue,
+        "The given action is not supported, please enter encode or decode."
+    );
     match action {
         "encode" => println!("Result:\n{}", coder.encode(string.as_ref())),
-        "decode" => println!("Result:\n{}", coder.decode(string.as_ref()).unwrap_or(Vec::from("[ctool] Unknown string."))
-            .iter()
-            .map(|&c| c as char)
-            .collect::<String>()),
-        _ => println!("[ctool] Action undefined."),
+        "decode" => {
+            match coder.decode(string.as_ref()) {
+                Ok(code) => {
+                    println!("Result:\n{}", code
+                        .iter()
+                        .map(|&c| c as char)
+                        .collect::<String>())
+                },
+                Err(_) => {
+                    let parse_error = command().error(
+                        clap::ErrorKind::InvalidValue,
+                        "The given string is not a base64 or hex."
+                    );
+                    parse_error.exit()
+                }
+            }
+        },
+        _ => action_undefined_error.exit(),
     }
 }
 
 fn encode_or_decode_url(action: &str, string: &str) {
+    let action_undefined_error = command().error(
+        clap::ErrorKind::InvalidValue,
+        "The given action is not supported, please enter encode or decode."
+    );
     match action {
         "encode" => println!("Result:\n{}", urlencoding::encode(string)),
         "decode" => println!("Result:\n{}", urlencoding::decode(string).unwrap()),
-        _ => println!("[ctool] Action undefined."),
+        _ => action_undefined_error.exit(),
     }
 }
 
+fn command() -> Command<'static> {
+    Command::new("ctool")
+}
+
 fn main() {
-    let app = Command::new("ctool");
-    let matches = &app
+    let matches = command()
         .version("0.1.0")
         .author("Evan Luo <ziyun.luo@icloud.com>")
         .about("A union tool covert base64, url, etc.")
@@ -47,15 +71,22 @@ fn main() {
             .help("The string waiting for decode or encode.")
             .required(true)
         )
-        .get_matches();
+        .try_get_matches_from(std::env::args_os())
+        .unwrap_or_else(|e| e.exit());
+
+    let type_undefined_error = command().error(
+        clap::ErrorKind::InvalidValue,
+        "The given type is not supported, please enter base64, hex, etc."
+    );
 
     let action = matches.value_of("action").unwrap();
     let string = matches.value_of("string").unwrap();
     let kind = matches.value_of("type").unwrap();
+
     match kind {
         "base64" => encode_or_decode(action, string, BASE64),
         "hex" => encode_or_decode(action, string, HEXLOWER),
         "url" => encode_or_decode_url(action, string),
-        _ => println!("Code type undefined."),
+        _ => type_undefined_error.exit(),
     }
 }
